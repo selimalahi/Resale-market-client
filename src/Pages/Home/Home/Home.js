6f6d631f06adf126b199e2eb9a68cb861ba29cdb
selimalahi <alahi.selim@gmail.com>
2022-11-24 14:38:07 +0600

    navbar update

---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Carousel from '../Carousel/Carousel';
+
+const Home = () => {
+    return (
+        <div className='mx-5'>
+            <Carousel></Carousel>
+        </div>
+    );
+};
+
+export default Home;
